d51d501819741d1c6209841178168b9392cc7d76
KateKrouli <krouli.e@mailbox366.com>
2025-12-02 15:47:41 +0000

    desktop

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,7 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
 	new Swiper('.locations-block__list.swiper', {
 		slidesPerView: 'auto',
 		spaceBetween: 20,
-		watchOverflow: false,
 		navigation: {
 			nextEl: '.locations-block__next',
 			prevEl: '.locations-block__prev',
--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,17 @@
 (() => {
+  // src/js/main.js
+  document.addEventListener("DOMContentLoaded", function() {
+    if (typeof Swiper === "undefined")
+      return;
+    new Swiper(".locations-block__list.swiper", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      watchOverflow: false,
+      navigation: {
+        nextEl: ".locations-block__next",
+        prevEl: ".locations-block__prev"
+      },
+      grabCursor: true
+    });
+  });
 })();
