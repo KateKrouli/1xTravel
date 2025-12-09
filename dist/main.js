26593a863a7bc070e01205812bb8cab8ff7afa5f
KateKrouli <krouli.e@mailbox366.com>
2025-12-09 14:08:28 +0000

    mobile

--- dist/main.js
+++ dist/main.js
@@ -14,4 +14,30 @@
       grabCursor: true
     });
   });
+  document.addEventListener("DOMContentLoaded", function() {
+    const headerNavIcon = document.querySelector(".header__nav-icon");
+    const headerContent = document.querySelector(".header__content");
+    const header = document.querySelector(".header");
+    if (headerNavIcon && headerContent) {
+      headerNavIcon.addEventListener("click", function(e) {
+        e.preventDefault();
+        console.log("Clicked nav icon");
+        header.classList.toggle("header--open");
+        headerContent.classList.toggle("header__content--open");
+        headerNavIcon.classList.toggle("header__nav-icon--open");
+      });
+    } else {
+      console.warn("Nav icon or content not found");
+    }
+    const navItems = document.querySelectorAll(".header__content .nav__item");
+    if (navItems && navItems.length) {
+      navItems.forEach(function(item) {
+        item.addEventListener("click", function() {
+          header.classList.remove("header--open");
+          headerContent.classList.remove("header__content--open");
+          headerNavIcon.classList.remove("header__nav-icon--open");
+        });
+      });
+    }
+  });
 })();
